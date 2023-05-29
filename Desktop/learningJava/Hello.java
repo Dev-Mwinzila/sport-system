496644fbdc2b8ae91a977ca8788ef7b7b71c9954
Lincohn Mwinzila <MwinzilaLincohn@outlook.com>
2023-05-29 23:03:28 +0300

    learning java , oop concepts  of inheritance , polymorphism methods, classes in java

---
+++ Desktop/learningJava/Hello.java
@@ -0,0 +1,20 @@
+public class Hello{
+    /**
+     * @param arg
+     */
+    public static void main(String arg[]){
+  int x=5;
+  int y=6;
+  int z=7;
+  if (x>y || x>z) {
+    
+  System.out.println("Lewis");
+  } else{
+    
+  System.out.println("Moi");
+  }
+ 
+
+    }
+   
+}
